ef1daba15128d68e52d689ba259df612fe6c1c70
Jason Fayre <jfayre@gmail.com>
2023-11-12 12:44:45 -0500

    Add project files.

---
+++ forzaDash/DashboardViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+
+namespace forzaDash
+{
+    public class DashboardViewModel : INotifyPropertyChanged
+    {
+        private float _rpm;
+        private float _tireTemperature;
+        private double _speed;
+        private int _lapNumber;
+        private int _position;
+
+        public float RPM
+        {
+            get => _rpm;
+            set
+            {
+                if (_rpm != value)
+                {
+                    _rpm = value;
+                    OnPropertyChanged(nameof(RPM));
+                }
+            }
+        }
+
+        public float TireTemperature
+        {
+            get => _tireTemperature;
+            set
+            {
+                if (_tireTemperature != value)
+                {
+                    _tireTemperature = value;
+                    OnPropertyChanged(nameof(TireTemperature));
+                }
+            }
+        }
+
+        public double Speed
+        {
+            get => _speed;
+            set
+            {
+                if (_speed != value)
+                {
+                    _speed = value;
+                    OnPropertyChanged(nameof(Speed));
+                }
+            }
+        }
+
+        public int LapNumber
+        {
+            get => _lapNumber;
+            set
+            {
+                if (_lapNumber != value)
+                {
+                    _lapNumber = value;
+                    OnPropertyChanged(nameof(LapNumber));
+                }
+            }
+        }
+
+        public int Position
+        {
+            get => _position;
+            set
+            {
+                if (_position != value)
+                {
+                    _position = value;
+                    OnPropertyChanged(nameof(Position));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ forzaDash/MainWindow.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Configuration;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ForzaDash;
+using SharpDX.DirectInput;
+using DavyKager;
+// using SharpDX.DirectInput;
+using SharpDX.XInput;
+
+namespace forzaDash
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool isRaceOn = false;
+        private static DataPacket data = new DataPacket();
+        private static double speedKPHRaw;
+        private static double speedKPH;
+        private float RPM;
+        private const int FORZA_DATA_OUT_PORT = 5300;
+        private const int FORZA_HOST_PORT = 5200;
+
+
+        private static double oldSpeed;
+        private DashboardViewModel _viewModel;
+        private State old;
+
+        public MainWindow()
+        {
+            _viewModel = new DashboardViewModel();
+            InitializeComponent();
+            this.DataContext = _viewModel;
+            
+            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, FORZA_DATA_OUT_PORT);
+            var senderClient = new UdpClient(FORZA_HOST_PORT);
+            Tolk.Load();
+            var c1 = new Controller(UserIndex.One);
+            
+            var receiverTask = Task.Run(async () =>
+            {
+                var client = new UdpClient(FORZA_DATA_OUT_PORT);
+                while (true)
+                {
+                    await client.ReceiveAsync().ContinueWith(receive =>
+                    {
+                        var resultBuffer = receive.Result.Buffer;
+                        if (!AdjustToBufferType(resultBuffer.Length))
+                        {
+                            Console.WriteLine("new-data", $"buffer not the correct length. length is {resultBuffer.Length}");
+                            return;
+                        }
+                        isRaceOn = resultBuffer.IsRaceOn();
+
+
+                        if (resultBuffer.IsRaceOn())
+                        {
+                            data = ParseData(resultBuffer);
+                            speedKPHRaw = data.Speed * 3.6;
+                            speedKPH = Math.Floor(speedKPHRaw);
+                            
+                            
+
+                            _viewModel.RPM = (float)Math.Floor(data.CurrentEngineRpm);
+                            _viewModel.TireTemperature = data.TireTempFl;
+                            _viewModel.Speed = speedKPH;
+                            State newState = c1.GetState();
+                            if (this.old.Gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb) == false && newState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb) == true)
+                            {
+                                Tolk.Output(speedKPH.ToString());
+                            }
+                            if (this.old.Gamepad.Buttons.HasFlag(GamepadButtonFlags.RightThumb) == false && newState.Gamepad.Buttons.HasFlag(GamepadButtonFlags.RightThumb))
+                            {
+                                Tolk.Output($" lap {data.Lap}, p {data.RacePosition.ToString()}");
+                            }
+                            
+                                this.old = newState;
+                            
+                            
+                        
+                    
+                    
+
+
+
+                        }
+                    });
+                }
+            });
+            // connection.Listen();
+
+
+
+        }
+
+        static DataPacket ParseData(byte[] packet)
+        {
+            DataPacket data = new DataPacket();
+
+            // sled
+            data.IsRaceOn = packet.IsRaceOn();
+            data.TimestampMS = packet.TimestampMs();
+            data.EngineMaxRpm = packet.EngineMaxRpm();
+            data.EngineIdleRpm = packet.EngineIdleRpm();
+            data.CurrentEngineRpm = packet.CurrentEngineRpm();
+            data.AccelerationX = packet.AccelerationX();
+            data.AccelerationY = packet.AccelerationY();
+            data.AccelerationZ = packet.AccelerationZ();
+            data.VelocityX = packet.VelocityX();
+            data.VelocityY = packet.VelocityY();
+            data.VelocityZ = packet.VelocityZ();
+            data.AngularVelocityX = packet.AngularVelocityX();
+            data.AngularVelocityY = packet.AngularVelocityY();
+            data.AngularVelocityZ = packet.AngularVelocityZ();
+            data.Yaw = packet.Yaw();
+            data.Pitch = packet.Pitch();
+            data.Roll = packet.Roll();
+            data.NormalizedSuspensionTravelFrontLeft = packet.NormSuspensionTravelFl();
+            data.NormalizedSuspensionTravelFrontRight = packet.NormSuspensionTravelFr();
+            data.NormalizedSuspensionTravelRearLeft = packet.NormSuspensionTravelRl();
+            data.NormalizedSuspensionTravelRearRight = packet.NormSuspensionTravelRr();
+            data.TireSlipRatioFrontLeft = packet.TireSlipRatioFl();
+            data.TireSlipRatioFrontRight = packet.TireSlipRatioFr();
+            data.TireSlipRatioRearLeft = packet.TireSlipRatioRl();
+            data.TireSlipRatioRearRight = packet.TireSlipRatioRr();
+            data.WheelRotationSpeedFrontLeft = packet.WheelRotationSpeedFl();
+            data.WheelRotationSpeedFrontRight = packet.WheelRotationSpeedFr();
+            data.WheelRotationSpeedRearLeft = packet.WheelRotationSpeedRl();
+            data.WheelRotationSpeedRearRight = packet.WheelRotationSpeedRr();
+            data.WheelOnRumbleStripFrontLeft = packet.WheelOnRumbleStripFl();
+            data.WheelOnRumbleStripFrontRight = packet.WheelOnRumbleStripFr();
+            data.WheelOnRumbleStripRearLeft = packet.WheelOnRumbleStripRl();
+            data.WheelOnRumbleStripRearRight = packet.WheelOnRumbleStripRr();
+            data.WheelInPuddleDepthFrontLeft = packet.WheelInPuddleFl();
+            data.WheelInPuddleDepthFrontRight = packet.WheelInPuddleFr();
+            data.WheelInPuddleDepthRearLeft = packet.WheelInPuddleRl();
+            data.WheelInPuddleDepthRearRight = packet.WheelInPuddleRr();
+            data.SurfaceRumbleFrontLeft = packet.SurfaceRumbleFl();
+            data.SurfaceRumbleFrontRight = packet.SurfaceRumbleFr();
+            data.SurfaceRumbleRearLeft = packet.SurfaceRumbleRl();
+            data.SurfaceRumbleRearRight = packet.SurfaceRumbleRr();
+            data.TireSlipAngleFrontLeft = packet.TireSlipAngleFl();
+            data.TireSlipAngleFrontRight = packet.TireSlipAngleFr();
+            data.TireSlipAngleRearLeft = packet.TireSlipAngleRl();
+            data.TireSlipAngleRearRight = packet.TireSlipAngleRr();
+            data.TireCombinedSlipFrontLeft = packet.TireCombinedSlipFl();
+            data.TireCombinedSlipFrontRight = packet.TireCombinedSlipFr();
+            data.TireCombinedSlipRearLeft = packet.TireCombinedSlipRl();
+            data.TireCombinedSlipRearRight = packet.TireCombinedSlipRr();
+            data.SuspensionTravelMetersFrontLeft = packet.SuspensionTravelMetersFl();
+            data.SuspensionTravelMetersFrontRight = packet.SuspensionTravelMetersFr();
+            data.SuspensionTravelMetersRearLeft = packet.SuspensionTravelMetersRl();
+            data.SuspensionTravelMetersRearRight = packet.SuspensionTravelMetersRr();
+            data.CarOrdinal = packet.CarOrdinal();
+            data.CarClass = packet.CarClass();
+            data.CarPerformanceIndex = packet.CarPerformanceIndex();
+            data.DrivetrainType = packet.DriveTrain();
+            data.NumCylinders = packet.NumCylinders();
+
+            // dash
+            data.PositionX = packet.PositionX();
+            data.PositionY = packet.PositionY();
+            data.PositionZ = packet.PositionZ();
+            data.Speed = packet.Speed();
+            data.Power = packet.Power();
+            data.Torque = packet.Torque();
+            data.TireTempFl = packet.TireTempFl();
+            data.TireTempFr = packet.TireTempFr();
+            data.TireTempRl = packet.TireTempRl();
+            data.TireTempRr = packet.TireTempRr();
+            data.Boost = packet.Boost();
+            data.Fuel = packet.Fuel();
+            data.Distance = packet.Distance();
+            data.BestLapTime = packet.BestLapTime();
+            data.LastLapTime = packet.LastLapTime();
+            data.CurrentLapTime = packet.CurrentLapTime();
+            data.CurrentRaceTime = packet.CurrentRaceTime();
+            data.Lap = packet.Lap();
+            data.RacePosition = packet.RacePosition();
+            data.Accelerator = packet.Accelerator();
+            data.Brake = packet.Brake();
+            data.Clutch = packet.Clutch();
+            data.Handbrake = packet.Handbrake();
+            data.Gear = packet.Gear();
+            data.Steer = packet.Steer();
+            data.NormalDrivingLine = packet.NormalDrivingLine();
+            data.NormalAiBrakeDifference = packet.NormalAiBrakeDifference();
+
+            return data;
+        }
+
+        static bool AdjustToBufferType(int bufferLength)
+        {
+            switch (bufferLength)
+            {
+                case 232: // FM7 sled
+                    return false;
+                case 311: // FM7 dash
+                    FMData.BufferOffset = 0;
+                    return true;
+                case 324: // FH4
+                    FMData.BufferOffset = 12;
+                    return true;
+                case 331: // FM8 dash
+                    FMData.BufferOffset = 0;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+    }
+}
